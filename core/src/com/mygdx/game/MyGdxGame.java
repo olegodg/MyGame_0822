15206342107198ee3ddf8cbb44051360af9ccffb
Oleg <odgoleg@gmail.com>
2022-08-19 16:27:00 +0300

    Initial commit

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,42 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	int clk;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("Green.jpg");
+
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0, 1, 1, 1);
+		float x = Gdx.input.getX()- img.getWidth()/2;
+		float y = Gdx.graphics.getHeight()-Gdx.input.getY() - img.getHeight()/2;
+		if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) clk++;
+		Gdx.graphics.setTitle("Clickes"+clk+"Tiimes");
+	// рисовалка
+		batch.begin();
+	//	batch.draw(img, 0, 0);
+		batch.draw(img,x,y);
+		batch.end();
+		//
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
